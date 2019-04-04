acda218026607a04398e420bd56489724d90c01e
Shivam Jindal <jshivam21@gmail.com>
2019-04-03 22:46:30 -0400

    initial code to insert/upload photo

--- insert_photo.py
+++ insert_photo.py
@@ -1,18 +1,26 @@
-from flask import render_template, request
-import os
-import time
-from app import  IMAGES_DIR, connection
+from app import IMAGES_DIR, connection, render_template, request, session, os, time
 
 
 def upload_image():
     if request.files:
         image_file = request.files.get("imageToUpload", "")
+        all_followers = int(request.form.get("allFollowers") != None)
         image_name = image_file.filename
         filepath = os.path.join(IMAGES_DIR, image_name)
         image_file.save(filepath)
-        query = "INSERT INTO photo (timestamp, filePath) VALUES (%s, %s)"
+
+        query = "INSERT INTO photo (photoOwner, timestamp, filePath, allFollowers) VALUES (%s, %s, %s, %s)"
         with connection.cursor() as cursor:
-            cursor.execute(query, (time.strftime('%Y-%m-%d %H:%M:%S'), image_name))
+            cursor.execute(query, (session["username"], time.strftime('%Y-%m-%d %H:%M:%S'), image_name, all_followers))
+            photo_id = cursor.lastrowid
+            # print("1234 EXEC", cursor._executed, "RESULT", photo_id)
+
+
+        # if not all_followers:
+        #     query = "INSERT INTO Share (groupName, groupOwner, photoID) VALUES (%s, %s, %s)"
+        #     with connection.cursor() as cursor:
+        #         cursor.execute(query, ())
+
         message = "Image has been successfully uploaded."
         return render_template("upload.html", message=message)
     else:
