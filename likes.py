b317d80e8880cd6e4cf15bd26184b4df8ad689ae
Shivam Jindal <jshivam21@gmail.com>
2019-04-27 16:20:19 -0400

    comments

--- likes.py
+++ likes.py
@@ -1,8 +1,8 @@
 from app import IMAGES_DIR, connection, render_template, request, session, os, time
 
 def submit_like():
-    request_data = request.form
-    photoid = request_data["photoid"]
-    query = "insert into liked (username, photoID) VALUES (%s, %s)"
+    request_data = request.form # get the proper form
+    photoid = request_data["photoid"] # get the photoid of the liked photo
+    query = "insert into liked (username, photoID) VALUES (%s, %s)" # prepare the query
     with connection.cursor() as cursor:
-            cursor.execute(query, (session["username"], photoid))
+            cursor.execute(query, (session["username"], photoid)) # submit the photo
